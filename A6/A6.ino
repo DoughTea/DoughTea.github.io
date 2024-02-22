c8faba334130827c641d674f8e8413702d1e92b6
Andrew Doty <dotyand@uw.edu>
2024-02-22 00:02:28 -0800

    Can't send data to arduino

---
+++ A6/A6.ino
@@ -0,0 +1,28 @@
+const int BUTTON_PIN = 2;
+const int SERVO_PIN = 9;
+// const String X_PIN = A0;
+// const String Y_PIN = A2;
+const int P5_PIN = 10;
+int incomingByte; // variable for reading from serial port - from p5.js 
+int led2brightness = 0; // set up another variable for visualize second LED's brightness
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(P5_PIN, OUTPUT);
+}
+
+void loop()
+{
+  Serial.println(digitalRead(BUTTON_PIN));
+  
+  if (Serial.read() == 3) {
+    digitalWrite(10, HIGH);
+  } else {
+    digitalWrite(10, LOW);
+  }
+
+  delay(50);
+}
