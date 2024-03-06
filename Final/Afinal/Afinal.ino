6ecb855cd0467a50429bfac7f19bfec7cdc4dcc1
Andrew Doty <dotyand@uw.edu>
2024-03-05 20:05:02 -0800

    finalHTML

--- Final/Afinal/Afinal.ino
+++ Final/Afinal/Afinal.ino
@@ -14,9 +14,6 @@ int BUTTON = 8;
 // initialize toggle for go and stop 
 bool toggled = false;
 
-// initialize reverse
-bool reverse = false;
-
 void setup() {
   // Set up serial Monitor for debugging
   Serial.begin(9600);
@@ -39,40 +36,37 @@ void setup() {
 void loop() {
   // Print if button is pressed for testing
   Serial.println(digitalRead(BUTTON));
-  
+  bool reverse = false;
   if (digitalRead(BUTTON) == 1) {
     toggled = !toggled;
   }
 
   while (toggled) {
-    // Run in figure 8 a couple times
-    for (int i = 0; i < 2; i++) {
-      // turn right
-      setMotorR(reverse);
-      setMotorL( reverse);
-      delay(500);
+    // turn right
+    setMotorR(!reverse);
+    setMotorL(reverse);
+    delay(500);
 
-      // forward
-      setMotorR(!reverse);
-      setMotorL(!reverse);
-      delay(500);
+    // forward
+    setMotorR(!reverse);
+    setMotorL(!reverse);
+    delay(5000);
 
-      // turn left
-      setMotorR(reverse);
-      setMotorL(!reverse);
-      delay(500);
+    // turn left
+    setMotorR(reverse);
+    setMotorL(!reverse);
+    delay(500);
 
-      // forward
-      setMotorR(!reverse);
-      setMotorL(!reverse);
-      delay(500);
-    }
+    // forward
+    setMotorR(!reverse);
+    setMotorL(!reverse);
+    delay(5000);  
   }
 }
 
 // Moves the right motor
 void setMotorR(bool reverse) {
-  digitalWrite(RIGHT_MOTOR, ! reverse);
+  digitalWrite(RIGHT_MOTOR, !reverse);
   digitalWrite(RIGHT_MOTOR2, reverse);
 }
 
